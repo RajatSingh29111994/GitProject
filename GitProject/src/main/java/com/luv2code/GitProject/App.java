65cbbaf7b16d264ad459f1e10f38d2815f4e7f74
RajatSingh29111994 <45950091+RajatSingh29111994@users.noreply.github.com>
2018-12-19 14:21:31 +0530

    Update App.java

--- GitProject/src/main/java/com/luv2code/GitProject/App.java
+++ GitProject/src/main/java/com/luv2code/GitProject/App.java
@@ -13,6 +13,7 @@ System.out.println( "Hello World! QA" );
 
 System.out.println( "Hello World! Environment" );
 System.out.println( "Hello World! Production" );
+System.out.println( "Hello World! UAT pull..." );
 
     }
 }
