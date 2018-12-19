501a7a4e2011267bdf8a5655a04930715926b815
user <user@UPLC>
2018-12-19 16:05:22 +0530

    n nj k

--- GitProject/src/main/java/com/luv2code/GitProject/TestJava.java
+++ GitProject/src/main/java/com/luv2code/GitProject/TestJava.java
@@ -5,7 +5,7 @@ public class TestJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("QA");
-System.out.println("Not Ignore");
+System.out.println("Not Ignore cgcgcfgfc");
 	}
 
 }
